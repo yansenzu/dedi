8ea841f7c7c3c11d92e4d53e0d2a97344561b112
yansenzu <koderead00@gmail.com>
2021-09-26 22:02:39 +0700

    add database sql

--- spk_smart.sql
+++ spk_smart.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2021 at 05:38 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- Generation Time: Sep 26, 2021 at 05:01 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -62,10 +63,8 @@ CREATE TABLE `alternatif` (
 --
 
 INSERT INTO `alternatif` (`id_alternatif`, `id_pelamar`, `pendidikan`, `usia`, `pengalaman`, `sertifikat_keahlian`, `test_tulis`, `wawancara`, `kesehatan`, `jumlah`) VALUES
-(1, '3', '85', '0', '85', '100', '0', '80', '80', '0.69690721649484'),
-(2, '4', '85', '100', '50', '100', '70', '80', '80', '0.78969072164948'),
-(3, '5', '40', '80', '20', '100', '30', '80', '80', '0.5701030927835'),
-(4, '6', '70', '100', '85', '100', '90', '80', '80', '0.86546391752577');
+(1, '3', '85', '0', '85', '100', '50', '90', '70', '0.69690721649484'),
+(3, '5', '0', '', '10', '100', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -77,11 +76,11 @@ CREATE TABLE `job_list` (
   `id_job` int(11) NOT NULL,
   `title` text NOT NULL,
   `konten` text NOT NULL,
-  `slug` text,
+  `slug` text DEFAULT NULL,
   `kategori` varchar(50) NOT NULL,
   `kualifikasi` varchar(250) NOT NULL,
   `tingkat_pekerjaan` varchar(250) NOT NULL,
-  `date_post` date DEFAULT NULL
+  `date_post` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -89,8 +88,11 @@ CREATE TABLE `job_list` (
 --
 
 INSERT INTO `job_list` (`id_job`, `title`, `konten`, `slug`, `kategori`, `kualifikasi`, `tingkat_pekerjaan`, `date_post`) VALUES
-(1, 'Sales Eksekutif', '<p>CV. Motorindo Perkasa</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p>Membutuhkan Sumber Daya Manusia yang memenuhi syarat berikut :</p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Seluruh Jurusan</li>\r\n	<li>Bersedia penempatan dan mobile di area Pekanbaru dan sekitar Riau</li>\r\n	<li>Memiliki relasi serta network yang luas</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah dengan Customer.</li>\r\n	<li>Sanggup untuk mencapai target yang telah ditetapkan oleh perusahaan</li>\r\n</ul>\r\n', '', 'Marketing', 'Sertifikat Professional, Pendidikan, Pengalaman', 'Sales', '2021-08-03'),
-(3, 'Staff Accounting', '<p>CV. Motorindo Perkasa</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p><strong>Kriteria :</strong></p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Jurusan Akuntansi</li>\r\n	<li>Memiliki pengalaman Minimal 2 Tahun&nbsp;</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah</li>\r\n</ul>\r\n', '', 'Finance', 'Sertifikat Professional, D3 Akunting, Pengalaman', 'Akuntan', '2021-08-15');
+(1, 'Engineering Development Trainee (EDT)', '<p>PT Riau Andalan Pulp and Paper Pekanbaru Ditayangkan pada 20 jam yang lalu Keuntungan We are leader in fibre, pulp, and paper industry Talent development programmes Employee welfare &amp; benefits (accomodation,transport,medical,etc) Deskripsi Pekerjaan EDT is our entry-level program that provides new engineering graduates with experiences, tools and support. By leveraging these resources, participants can meaningfully contribute to our business development efforts, especially in supporting our manufacturing environments. Bachelor &amp; Master Degree from these majors are welcome to apply : Chemical Engineering Mechanical Engineering Industrial Engineering Environmental Engineering Mechatronics Engineering Physics &amp; Instrumentation Informatics Engineering, System Information</p>\r\n', '', 'Engineering', 'Sarjana (S1), Diploma Pascasarjana, Gelar Professional, Magister (S2)', 'Lulusan baru/Pengalaman kerja kurang dari 1 tahun', '2021-08-03'),
+(3, 'Kepala Operasional Travel Shuttle', '<p>PT Agung Solusi Trans</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p>Ditayangkan pada 6 jam yang lalu</p>\r\n\r\n<p>Deskripsi Pekerjaan</p>\r\n\r\n<p><strong>Deskripsi Pekerjaan :</strong></p>\r\n\r\n<ul>\r\n	<li>Menyusun, mempersiapkan dan atau menentukan strategi marketing dan promosi.</li>\r\n	<li>Menyusun dan menentukan standar operasional kerja.</li>\r\n	<li>Menyusun laporan operasional kerja dan produktifitas untuk kebutuhan manajemen.</li>\r\n	<li>Mengelola dan menangani permintaan konsumen sesuai dengan paket yang telah tersedia</li>\r\n	<li>Menyusun dan mengelola jadwal perjalanan travel shuttle bagi driver dan dispatcher</li>\r\n	<li>Menjalin kerja sama yang baik dengan instansi, principal dan asosiasi yang terkait, yang dapat dimanfaatkan untuk meningkatkan penjualan dan keuntungan perusahaan</li>\r\n	<li>Memantau dan memecahkan masalah yang ada kaitannya dengan penurunan hasil penjualan jasa travel shuttle.</li>\r\n	<li>Bertanggung jawab dalam pengurusan dokumen-dokumen perjalanan.</li>\r\n</ul>\r\n\r\n<p><strong>Kriteria :</strong></p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Seluruh Jurusan</li>\r\n	<li>Memiliki pengalaman Minimal 3 Tahun pada Travel Shuttle Services</li>\r\n	<li>Bersedia penempatan dan mobile di area Pekanbaru dan sekitar Riau</li>\r\n	<li>Memiliki relasi serta network yang luas</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah dengan Customer.</li>\r\n	<li>Terampil menggunakan aplikasi seperti Photoshop dan Office</li>\r\n	<li>Memiliki SIM A</li>\r\n	<li>Sanggup untuk mencapai target yang telah ditetapkan oleh perusahaan</li>\r\n</ul>\r\n', '', 'Marketing', 'Sertifikat Professional, D3 (Diploma), D4 (Diploma)', 'Supervisor/Koordinator', '2021-08-03'),
+(4, 'Kepala Operasional Travel Shuttle', '<p>PT Agung Solusi Trans</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p>Ditayangkan pada 6 jam yang lalu</p>\r\n\r\n<p>Deskripsi Pekerjaan</p>\r\n\r\n<p><strong>Deskripsi Pekerjaan :</strong></p>\r\n\r\n<ul>\r\n	<li>Menyusun, mempersiapkan dan atau menentukan strategi marketing dan promosi.</li>\r\n	<li>Menyusun dan menentukan standar operasional kerja.</li>\r\n	<li>Menyusun laporan operasional kerja dan produktifitas untuk kebutuhan manajemen.</li>\r\n	<li>Mengelola dan menangani permintaan konsumen sesuai dengan paket yang telah tersedia</li>\r\n	<li>Menyusun dan mengelola jadwal perjalanan travel shuttle bagi driver dan dispatcher</li>\r\n	<li>Menjalin kerja sama yang baik dengan instansi, principal dan asosiasi yang terkait, yang dapat dimanfaatkan untuk meningkatkan penjualan dan keuntungan perusahaan</li>\r\n	<li>Memantau dan memecahkan masalah yang ada kaitannya dengan penurunan hasil penjualan jasa travel shuttle.</li>\r\n	<li>Bertanggung jawab dalam pengurusan dokumen-dokumen perjalanan.</li>\r\n</ul>\r\n\r\n<p><strong>Kriteria :</strong></p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Seluruh Jurusan</li>\r\n	<li>Memiliki pengalaman Minimal 3 Tahun pada Travel Shuttle Services</li>\r\n	<li>Bersedia penempatan dan mobile di area Pekanbaru dan sekitar Riau</li>\r\n	<li>Memiliki relasi serta network yang luas</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah dengan Customer.</li>\r\n	<li>Terampil menggunakan aplikasi seperti Photoshop dan Office</li>\r\n	<li>Memiliki SIM A</li>\r\n	<li>Sanggup untuk mencapai target yang telah ditetapkan oleh perusahaan</li>\r\n</ul>\r\n', '<p>PT Agung Solusi Trans</p>\r\n...', 'Marketing', 'Sertifikat Professional, D3 (Diploma), D4 (Diploma)', 'Supervisor/Koordinator', '2021-08-03'),
+(5, 'Kepala Operasional Travel Shuttle', '<p>PT Agung Solusi Trans</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p>Ditayangkan pada 6 jam yang lalu</p>\r\n\r\n<p>Deskripsi Pekerjaan</p>\r\n\r\n<p><strong>Deskripsi Pekerjaan :</strong></p>\r\n\r\n<ul>\r\n	<li>Menyusun, mempersiapkan dan atau menentukan strategi marketing dan promosi.</li>\r\n	<li>Menyusun dan menentukan standar operasional kerja.</li>\r\n	<li>Menyusun laporan operasional kerja dan produktifitas untuk kebutuhan manajemen.</li>\r\n	<li>Mengelola dan menangani permintaan konsumen sesuai dengan paket yang telah tersedia</li>\r\n	<li>Menyusun dan mengelola jadwal perjalanan travel shuttle bagi driver dan dispatcher</li>\r\n	<li>Menjalin kerja sama yang baik dengan instansi, principal dan asosiasi yang terkait, yang dapat dimanfaatkan untuk meningkatkan penjualan dan keuntungan perusahaan</li>\r\n	<li>Memantau dan memecahkan masalah yang ada kaitannya dengan penurunan hasil penjualan jasa travel shuttle.</li>\r\n	<li>Bertanggung jawab dalam pengurusan dokumen-dokumen perjalanan.</li>\r\n</ul>\r\n\r\n<p><strong>Kriteria :</strong></p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Seluruh Jurusan</li>\r\n	<li>Memiliki pengalaman Minimal 3 Tahun pada Travel Shuttle Services</li>\r\n	<li>Bersedia penempatan dan mobile di area Pekanbaru dan sekitar Riau</li>\r\n	<li>Memiliki relasi serta network yang luas</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah dengan Customer.</li>\r\n	<li>Terampil menggunakan aplikasi seperti Photoshop dan Office</li>\r\n	<li>Memiliki SIM A</li>\r\n	<li>Sanggup untuk mencapai target yang telah ditetapkan oleh perusahaan</li>\r\n</ul>\r\n', '<p>PT Agung Solusi Trans</p>\r\n...', 'Marketing', 'Sertifikat Professional, D3 (Diploma), D4 (Diploma)', 'Supervisor/Koordinator', '2021-08-03'),
+(6, 'Kepala Operasional Travel Shuttle', '<p>PT Agung Solusi Trans</p>\r\n\r\n<p>Pekanbaru</p>\r\n\r\n<p>Ditayangkan pada 6 jam yang lalu</p>\r\n\r\n<p>Deskripsi Pekerjaan</p>\r\n\r\n<p><strong>Deskripsi Pekerjaan :</strong></p>\r\n\r\n<ul>\r\n	<li>Menyusun, mempersiapkan dan atau menentukan strategi marketing dan promosi.</li>\r\n	<li>Menyusun dan menentukan standar operasional kerja.</li>\r\n	<li>Menyusun laporan operasional kerja dan produktifitas untuk kebutuhan manajemen.</li>\r\n	<li>Mengelola dan menangani permintaan konsumen sesuai dengan paket yang telah tersedia</li>\r\n	<li>Menyusun dan mengelola jadwal perjalanan travel shuttle bagi driver dan dispatcher</li>\r\n	<li>Menjalin kerja sama yang baik dengan instansi, principal dan asosiasi yang terkait, yang dapat dimanfaatkan untuk meningkatkan penjualan dan keuntungan perusahaan</li>\r\n	<li>Memantau dan memecahkan masalah yang ada kaitannya dengan penurunan hasil penjualan jasa travel shuttle.</li>\r\n	<li>Bertanggung jawab dalam pengurusan dokumen-dokumen perjalanan.</li>\r\n</ul>\r\n\r\n<p><strong>Kriteria :</strong></p>\r\n\r\n<ul>\r\n	<li>Pendidikan Minimal D3 Seluruh Jurusan</li>\r\n	<li>Memiliki pengalaman Minimal 3 Tahun pada Travel Shuttle Services</li>\r\n	<li>Bersedia penempatan dan mobile di area Pekanbaru dan sekitar Riau</li>\r\n	<li>Memiliki relasi serta network yang luas</li>\r\n	<li>Memiliki kemampuan leadership yang baik</li>\r\n	<li>Mampu berkomunikasi dengan baik, supel dan ramah dengan Customer.</li>\r\n	<li>Terampil menggunakan aplikasi seperti Photoshop dan Office</li>\r\n	<li>Memiliki SIM A</li>\r\n	<li>Sanggup untuk mencapai target yang telah ditetapkan oleh perusahaan</li>\r\n</ul>\r\n', '<p>PT Agung Solusi Trans</p>\r\n...', 'Marketing', 'Sertifikat Professional, D3 (Diploma), D4 (Diploma)', 'Supervisor/Koordinator', '2021-08-03');
 
 -- --------------------------------------------------------
 
@@ -147,17 +149,20 @@ CREATE TABLE `pelamar` (
   `kategori` varchar(20) DEFAULT NULL,
   `test_tulis` varchar(20) DEFAULT NULL,
   `wawancara` varchar(20) DEFAULT NULL,
-  `kesehatan` varchar(20) DEFAULT NULL
+  `kesehatan` varchar(20) DEFAULT NULL,
+  `keterangan` varchar(30) DEFAULT NULL,
+  `catatan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pelamar`
 --
 
-INSERT INTO `pelamar` (`id_pelamar`, `nik`, `password`, `nama`, `email`, `alamat`, `tmp_lahir`, `tgl_lahir`, `umur`, `hp`, `pendidikan`, `pengalaman`, `cv`, `foto`, `sertifikat_keahlian`, `ijazah`, `surat_kesehatan`, `id_job`, `kategori`, `test_tulis`, `wawancara`, `kesehatan`) VALUES
-(4, '1210110475', '123456', 'Dedi Kurnia', 'zaidediputra@gmail.com', 'Pekanbaru', 'Pekanbaru', '1993-11-18', 26, '', 'S1', 2, 'CV.pdf', 'd2.jpg', 'Sertifikat Keahlian.pdf', 'Ijazah1.pdf', 'Surat Kesehatan.pdf', '1', 'Marketing', '70', '80', '80'),
-(5, '13121415', '123456', 'Kurnia Zai', 'dedi.kurnia@student.pelitaindonesia.ac.id', 'Pekanbaru', 'Pekanbaru', '1998-02-18', 22, '', 'SMA', 1, 'CV.pdf', 'd3.jpg', 'Sertifikat Keahlian.pdf', 'Ijazah1.pdf', 'Surat Kesehatan.pdf', '1', 'Marketing', '30', '80', '80'),
-(6, '14121315', '123456', 'Tasya Aurora', 'aurora@gmail.com', 'Pekanbaru', 'Pekanbaru', '0005-12-11', 26, '+6281362289279', 'D3', 4, 'CV.pdf', 'agent2.jpg', 'Sertifikat Keahlian.pdf', 'Ijazah1.pdf', 'Surat Kesehatan.pdf', '1', 'Marketing', '90', '75', '100');
+INSERT INTO `pelamar` (`id_pelamar`, `nik`, `password`, `nama`, `email`, `alamat`, `tmp_lahir`, `tgl_lahir`, `umur`, `hp`, `pendidikan`, `pengalaman`, `cv`, `foto`, `sertifikat_keahlian`, `ijazah`, `surat_kesehatan`, `id_job`, `kategori`, `test_tulis`, `wawancara`, `kesehatan`, `keterangan`, `catatan`) VALUES
+(1, '133311331', '', 'Dedi', 'dedi@gmail.com', 'Pekanbaru', 'Nias', '1998-05-23', 23, '081232323232', 'D3', 2, NULL, 'xx', '', '', '', '6', NULL, NULL, '', '', 'Tidak lulus verifikasi', 'Silahkan upload cv terbaru'),
+(3, '0852', '12345', 'Eka Putra Yansen', 'ekayansen00@gmail.com', 'jalan melur gang nenas no 17 a', 'sungai pakning', '2000-05-01', 0, '+6282246657878', 'S1', 4, 'ARTIKEL.pdf', '120750735_1462196007324476_8514950370247336652_n.jpg', 'ARTIKEL.pdf', 'ARTIKEL.pdf', 'ARTIKEL.pdf', '5', 'Marketing', '50', NULL, NULL, 'Tidak lulus verifikasi', 'Silahkan upload cv terbaru'),
+(4, '123', '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Tidak lulus verifikasi', 'Silahkan upload cv terbaru'),
+(5, '999', '999', 'Dedi Konbanwa', 'domain@email.com', 'jalan semangka', 'pekanbaru', '2021-09-06', 0, '+6282246657878', 'Kurang dari SMA', 0, '1080360.png', '120750735_1462196007324476_8514950370247336652_n.jpg', '120834023_3346108888771299_4483232134398728129_o.jpg', '120834079_2553480974942128_3945248598538492789_o.jpg', '130547355_3661520990596240_2332021204399978968_n.jpg', NULL, NULL, NULL, NULL, NULL, 'Tidak lulus verifikasi', 'Silahkan upload cv terbaru');
 
 -- --------------------------------------------------------
 
@@ -236,8 +241,7 @@ INSERT INTO `tbl_soal` (`id_soal`, `soal`, `a`, `b`, `c`, `d`, `knc_jawaban`) VA
 (6, 'Deret Angka : 192 192 96 32 ', '4', '6', '8', '10', 'c'),
 (7, 'Jika Jakarta adalah kuda, surabaya adalah sapi, dan makasar adalah', 'Ayam', 'Kelelawar', 'Harimau', 'Gelatik', 'c'),
 (8, 'Nilai siapakah yang lebih tinggi dari soal berikut di bawah: Nilai Bima lebih jelek dari nilai Ema. Nilai Dian lebih bagus dari nilai Ali. Nilai Bima lebih bagus dari nilai Citra. Nilai Adit sama besar seperti nilai Bima. Nilai Ema lebih kecil dari nilai Dian', 'Baju Budi', 'Baju Andi', 'Baju Charly', 'Baju Dani', 'a'),
-(9, 'Bunga manakah yang paling indah dari soal berikut: Bunga Melati sama indahnya seperti bunga Mawar. Bunga Bakung tidak seindah bunga melati. Bunga Mawar tidak seindah bunga sakura. Bunga Melati lebih indah dari bunga Bakung', 'Mawar', 'Bakung', 'Melati', 'Sakura', 'd'),
-(10, 'Semua pekerja harus mengenakan topi pengaman. Sementara pekerja mengenakan sarung tangan.', 'Sementara pekerja tidak mengenakan topi pengaman', 'Semua pekerja tidak mengenakan sarung tangan', 'Sementara pekerja mengenakan topi pengaman dan sarung tangan', 'Sementara pekerja tidak mengenakan topi pengaman dan mengenakan sarung tangan', 'c');
+(9, 'Bunga manakah yang paling indah dari soal berikut: Bunga Melati sama indahnya seperti bunga Mawar. Bunga Bakung tidak seindah bunga melati. Bunga Mawar tidak seindah bunga sakura. Bunga Melati lebih indah dari bunga Bakung', 'Mawar', 'Bakung', 'Melati', 'Sakura', 'd');
 
 -- --------------------------------------------------------
 
@@ -329,42 +333,51 @@ ALTER TABLE `usia`
 -- AUTO_INCREMENT for table `alternatif`
 --
 ALTER TABLE `alternatif`
-  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `job_list`
 --
 ALTER TABLE `job_list`
-  MODIFY `id_job` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_job` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
   MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT for table `pelamar`
 --
 ALTER TABLE `pelamar`
-  MODIFY `id_pelamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pelamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `pendidikan`
 --
 ALTER TABLE `pendidikan`
   MODIFY `id_pendidikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `pengalaman`
 --
 ALTER TABLE `pengalaman`
   MODIFY `id_pengalaman` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `tbl_soal`
 --
 ALTER TABLE `tbl_soal`
   MODIFY `id_soal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `usia`
 --
 ALTER TABLE `usia`
   MODIFY `id_usia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
